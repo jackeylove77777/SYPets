edf8b2d5c49489ea2ef47e6af821d6dcc1ab5f98
jackeylove77777 <776109620@qq.com>
2023-02-21 15:26:30 +0800

    测试mysql连接

--- src/test/java/com/hth/TestMain.java
+++ src/test/java/com/hth/TestMain.java
@@ -1,12 +1,15 @@
 package com.hth;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hth.entity.Type;
 import com.hth.mapper.TypeMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class TestMain {
@@ -15,6 +18,9 @@ public class TestMain {
 
     @Test
     public void testTypeMapper(){
-
+        LambdaQueryWrapper<Type> wrapper = new LambdaQueryWrapper<>();
+        wrapper.orderByDesc(Type::getId);
+        List<Type> types = typeMapper.selectList(wrapper);
+        types.forEach(System.out::println);
     }
 }
